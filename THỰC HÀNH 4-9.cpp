d1d9307763cfbdf7f4b8f09f4f78e8068290e47c
thanhtainguyen <taintuitktmt@gmail.com>
2023-11-05 16:14:37 +0700

    codebasic

---
+++ THỰC HÀNH 4-9.cpp
@@ -0,0 +1,19 @@
+﻿//Nhập vào số nguyên dương n từ bàn phím.
+//Kiểm tra xem n có phải là số chính phương hay không ? (số chính phương là số khi lấy căn bặc 2 có kết quả là nguyên).
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Nhap so nguyen duong n: "; cin >> n;
+
+	float N = sqrt(n);
+
+	if (N - int(N) == 0) //hoặc N*N == n => so chinh phuong
+		cout << "Ket qua: n la so chinh phuong";
+	else
+		cout << "Ket qua: n ko la so chinh phuong";
+}
